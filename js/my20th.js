786c209d4345184444ce1f9e004451f23f6f6ca3
johson feng <fengyinchao1986@gmail.com>
2024-02-28 00:34:36 +0900

    20th

---
+++ js/my20th.js
@@ -0,0 +1,284 @@
+$(function(){	
+		$('#my').click(function(){
+			$('.dialog').fadeIn('slow');			
+		});
+		
+		$('.icon').click(function(){
+			$('.dialog').fadeOut('slow');		
+		});		
+		$('.pc').click(function(){
+			$('.dialog').hide(1000);			
+		});	
+		$('.friend').click(function(){
+			$('.pink').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.pink').hide(1000);			
+		});	
+		$('.button div:not(.friend):not(.red)').click(function(){
+			$('.pink').hide(1000);			
+		});	
+		$('.joblabel').click(function(){
+			$('.blue').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.blue').hide(1000);			
+		});	
+		$('.button div:not(.joblabel):not(.red)').click(function(){
+			$('.blue').hide(1000);			
+		});	
+		$('.workplace').click(function(){
+			$('.nave').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.nave').hide(1000);			
+		});	
+		$('.button div:not(.workplace):not(.red)').click(function(){
+			$('.nave').hide(1000);			
+		});	
+			$('.home').click(function(){
+			$('.brown').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.brown').hide(1000);			
+		});	
+		$('.button div:not(.home):not(.red)').click(function(){
+			$('.brown').hide(1000);			
+		});	
+			$('.social').click(function(){
+			$('.orange').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.orange').hide(1000);			
+		});	
+		$('.button div:not(.social):not(.red)').click(function(){
+			$('.orange').hide(1000);			
+		});	
+		$('.life').click(function(){
+			$('.green').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.green').hide(1000);			
+		});	
+		$('.button div:not(.life):not(.red)').click(function(){
+			$('.green').hide(1000);			
+		});	
+		
+		$('.font').click(function(){
+			$('#slick').fadeIn('slow');		
+		});
+		$('.font').click(function(){
+			$('.dialog').fadeOut('slow');			
+		});	
+		$('.people').click(function(){
+			$('#slick').fadeOut('slow');	
+		});	
+
+	});$(function(){	
+		$('#my').click(function(){
+			$('.dialog').fadeIn('slow');			
+		});
+		
+		$('.icon').click(function(){
+			$('.dialog').fadeOut('slow');		
+		});		
+		$('.pc').click(function(){
+			$('.dialog').hide(1000);			
+		});	
+		$('.friend').click(function(){
+			$('.pink').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.pink').hide(1000);			
+		});	
+		$('.button div:not(.friend):not(.red)').click(function(){
+			$('.pink').hide(1000);			
+		});	
+		$('.joblabel').click(function(){
+			$('.blue').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.blue').hide(1000);			
+		});	
+		$('.button div:not(.joblabel):not(.red)').click(function(){
+			$('.blue').hide(1000);			
+		});	
+		$('.workplace').click(function(){
+			$('.nave').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.nave').hide(1000);			
+		});	
+		$('.button div:not(.workplace):not(.red)').click(function(){
+			$('.nave').hide(1000);			
+		});	
+			$('.home').click(function(){
+			$('.brown').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.brown').hide(1000);			
+		});	
+		$('.button div:not(.home):not(.red)').click(function(){
+			$('.brown').hide(1000);			
+		});	
+			$('.social').click(function(){
+			$('.orange').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.orange').hide(1000);			
+		});	
+		$('.button div:not(.social):not(.red)').click(function(){
+			$('.orange').hide(1000);			
+		});	
+		$('.life').click(function(){
+			$('.green').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.green').hide(1000);			
+		});	
+		$('.button div:not(.life):not(.red)').click(function(){
+			$('.green').hide(1000);			
+		});	
+		
+		$('.font').click(function(){
+			$('#slick').fadeIn('slow');		
+		});
+		$('.font').click(function(){
+			$('.dialog').fadeOut('slow');			
+		});	
+		$('.people').click(function(){
+			$('#slick').fadeOut('slow');	
+		});	
+
+	});$(function(){	
+		$('#my').click(function(){
+			$('.dialog').fadeIn('slow');			
+		});
+		
+		$('.icon').click(function(){
+			$('.dialog').fadeOut('slow');		
+		});		
+		$('.pc').click(function(){
+			$('.dialog').hide(1000);			
+		});	
+		$('.friend').click(function(){
+			$('.pink').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.pink').hide(1000);			
+		});	
+		$('.button div:not(.friend):not(.red)').click(function(){
+			$('.pink').hide(1000);			
+		});	
+		$('.joblabel').click(function(){
+			$('.blue').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.blue').hide(1000);			
+		});	
+		$('.button div:not(.joblabel):not(.red)').click(function(){
+			$('.blue').hide(1000);			
+		});	
+		$('.workplace').click(function(){
+			$('.nave').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.nave').hide(1000);			
+		});	
+		$('.button div:not(.workplace):not(.red)').click(function(){
+			$('.nave').hide(1000);			
+		});	
+			$('.home').click(function(){
+			$('.brown').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.brown').hide(1000);			
+		});	
+		$('.button div:not(.home):not(.red)').click(function(){
+			$('.brown').hide(1000);			
+		});	
+			$('.social').click(function(){
+			$('.orange').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.orange').hide(1000);			
+		});	
+		$('.button div:not(.social):not(.red)').click(function(){
+			$('.orange').hide(1000);			
+		});	
+		$('.life').click(function(){
+			$('.green').show(1000);			
+		});
+		
+		$('.icon').click(function(){
+			$('.green').hide(1000);			
+		});	
+		$('.button div:not(.life):not(.red)').click(function(){
+			$('.green').hide(1000);			
+		});	
+		
+		$('.font').click(function(){
+			$('#slick').fadeIn('slow');		
+		});
+		$('.font').click(function(){
+			$('.dialog').fadeOut('slow');			
+		});	
+		$('.people').click(function(){
+			$('#slick').fadeOut('slow');	
+		});	
+
+	});var mySwiper = new Swiper ('.swiper-container', {
+  effect: "flip",
+  loop: true,
+  pagination: '.swiper-pagination',
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+})var mySwiper = new Swiper ('.swiper-container', {
+  effect: "flip",
+  loop: true,
+  pagination: '.swiper-pagination',
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+})var mySwiper = new Swiper ('.swiper-container', {
+  effect: "flip",
+  loop: true,
+  pagination: '.swiper-pagination',
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+})var mySwiper = new Swiper ('.swiper-container', {
+  effect: "flip",
+  loop: true,
+  pagination: '.swiper-pagination',
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+})var mySwiper = new Swiper ('.swiper-container', {
+  effect: "flip",
+  loop: true,
+  pagination: '.swiper-pagination',
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+})// JavaScript Document
+var mySwiper = new Swiper ('.swiper-container', {
+  effect: "flip",
+  loop: true,
+  pagination: '.swiper-pagination',
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+})
